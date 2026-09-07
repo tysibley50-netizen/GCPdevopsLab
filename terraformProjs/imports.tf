4241f3cc6b7d951bda0f0ae983723244d22a31a2
tysibley50-netizen <tysibley50@gmail.com>
2026-09-07 08:36:28 -0400

    Worked through an imports.tf specific issue with terraform plan. Did not get much in the way of pipeline built but understood better the format of HCL.

--- terraformProjs/imports.tf
+++ terraformProjs/imports.tf
@@ -2,10 +2,10 @@
 
 import {
   # The type of resource and what you want to name it in your code
-  to = google_cloud_run_v2_service.https://tiny-clickops-service-871657323406.us-east1.run.app
+  to = google_cloud_run_v2_service.tiny-clickops-service-871657323406
 
   # The exact ID string from your GCP Console
-  id = "projects/project-1f03d34e-6e19-45e0-a6a/locations/us-east/services/tiny-clickops-service.my_service"
+  id = "projects/project-1f03d34e-6e19-45e0-a6a/locations/us-east/services/tiny-clickops-service-871657323406.us-east1.run.app"
 }
 
 
